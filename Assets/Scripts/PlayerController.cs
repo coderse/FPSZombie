fab47b96ee2750677fd5f809fe7cf5212db532d1
Ahmet Keskin <ahmetkeskin.se@gmail.com>
2022-07-13 00:17:11 +0300

    Added Movement Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Player Control Settings")]
+    [SerializeField] private float walkSpeed = 8f;
+    [SerializeField] private float runSpeed = 12f;
+
+    private CharacterController characterController;
+
+    private float currentSpeed = 8f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    /***************************************************************/
+
+    private void Awake()
     {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        KeyboardInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 localVerticalVector = transform.forward * verticalInput;
+        Vector3 localHorizontalVector = transform.right * horizontalInput;
+
+        Vector3 movementVector = localHorizontalVector + localVerticalVector;
+        movementVector.Normalize();
+        movementVector *= currentSpeed * Time.deltaTime;
+
+        characterController.Move(movementVector);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void KeyboardInput()
     {
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = runSpeed;
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+        }
     }
 }
